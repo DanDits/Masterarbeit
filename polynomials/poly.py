40dc5b30367a1ffc883bef6655660410d2fd5755
DanDits <Da.dittmar@t-online.de>
2016-10-24 19:48:37 +0200

    Polys defined by general three term recursion

--- polynomials/poly.py
+++ polynomials/poly.py
@@ -1,4 +1,5 @@
 import math
+from functools import lru_cache
 
 
 def _make_distribution(name, is_continuous, gpc_basis_polynomial_name, support):
@@ -31,49 +32,45 @@ attribute_polybasis = "polybasis"
 attribute_polybasis_normalization_gamma = "polybasis_normalization_gamma"
 
 
-# Gaussian - additional attributes
-distributions["Gaussian"][attribute_weight] = lambda x: math.exp(-x*x/2.) / math.sqrt(2.*math.pi)
+# Pretty general implementation for a recursively defined polynomial basis, but not optimized as terms that
+# cancel out are still calculated and may lead to inaccuracies. For higher accuracy (for low order terms) use more hand
+# calculated polynomials for "polys_start"
+def _poly_basis_recursive(polys_start, recursion_factors):
+    """
+    Factory function for a basis of polynomials p(x) for a single double variable x.
+    The returned function takes a non negative integer n and returns a polynomial of degree n
+    which is the n-th basis polynomial. The first basis polynomials are given as function by polys_start.
+    The following polynomials are defined by the recursion factors f as follows:
+    p_n(x) = f_0(n,x) * p_(n-1)(x) + f_1(n,x) * p_(n-2)(x) + ...
+    :param polys_start: The m starting polynomials.
+    :param recursion_factors: factors as functions from degree n and position x,
+    defining the recursion for higher order polynomials.
+    :return: A function that takes an order n and returns a polynomial p mapping a double to a double.
+    Polynomials are cached internally but not mathematically simplified and not optimized.
+    """
+
+    @lru_cache(maxsize=None)
+    def poly(n):
+        if 0 <= n < len(polys_start):
+            return polys_start[n]
+        elif n >= len(polys_start):
+            return lambda x: sum(f_a(n, x) * poly(n - 1 - i)(x) for i, f_a in enumerate(recursion_factors))
+        else:
+            raise ValueError("Illegal degree " + n + " for polynomial basis function.")
+
+    return poly
+
+
+# Gaussian - additional attributes, recursion p_n(x)=x*p_(n-1)(x)-(n-1)*p_(n-2), p_0(x)=1, p_1(x)=x
+distributions["Gaussian"][attribute_weight] = lambda x: math.exp(-x * x / 2.) / math.sqrt(2. * math.pi)
 distributions["Gaussian"][attribute_polybasis_normalization_gamma] = lambda n: math.factorial(n)
+distributions["Gaussian"][attribute_polybasis] = _poly_basis_recursive((lambda x: 1, lambda x: x),  # TODO more polys
+                                                                       (lambda n, x: x, lambda n, x: 1 - n))
 
-
-def hermite_poly(n):
-    if n == 0:
-        return lambda z: 1
-    elif n == 1:
-        return lambda z: z
-    elif n == 2:
-        return lambda z: z*z-1
-    elif n == 3:
-        return lambda z: z*z*z - 3*z
-    elif n >= 2:  # General case (would also apply for n==2 and n==3)
-        prev = hermite_poly(n-1)
-        pre_prev = hermite_poly(n-2)
-        return lambda z: z*prev(z) - (n-1)*pre_prev(z)
-    else:
-        raise ValueError("Illegal degree " + n + " for polynomial basis function.")
-
-distributions["Gaussian"][attribute_polybasis] = hermite_poly
-
-
-# Uniform - additional attributes, interval assumed to be [-1,1]
+# Uniform - additional attributes,
+# interval assumed to be [-1,1], recursion p_n(x)=x(2n-1)/n * p_(n-1)(x)-(n-1)/n*p_(n-2)(x)
 distributions["Uniform"][attribute_weight] = lambda x: 0.5
-distributions["Uniform"][attribute_polybasis_normalization_gamma] = lambda n: 2/(2*n+1)
-
-
-def legendre_poly(n):
-    if n == 0:
-        return lambda z: 1
-    elif n == 1:
-        return lambda z: z
-    elif n == 2:
-        return lambda z: 0.5*(3*z*z-1)
-    elif n == 3:
-        return lambda z: 0.5*(5*z*z*z-3*z)
-    elif n >= 2:  # General case (would also apply for n==2 and n==3)
-        prev = hermite_poly(n - 1)
-        pre_prev = hermite_poly(n - 2)
-        return lambda z: ((2.*n-1)*z*prev(z) - (n-1)*pre_prev(z)) / n
-    else:
-        raise ValueError("Illegal degree " + n + " for polynomial basis function.")
-
-distributions["Gaussian"][attribute_polybasis] = legendre_poly
+distributions["Uniform"][attribute_polybasis_normalization_gamma] = lambda n: 2 / (2 * n + 1)
+distributions["Uniform"][attribute_polybasis] = _poly_basis_recursive((lambda x: 1, lambda x: x),  # TODO more polys
+                                                                      (lambda n, x: (2. * n - 1) / n * x,
+                                                                           lambda n, x: (1. - n) / n))
