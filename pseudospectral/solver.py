6b63e77925bfea4ecf34fdba26f060e33c774eb3
DanDits <Da.dittmar@t-online.de>
2016-11-01 14:24:36 +0100

    Multidimensional solver for heat equation. One dimensional solver for wave equation.

--- pseudospectral/solver.py
+++ pseudospectral/solver.py
@@ -1,8 +1,9 @@
 import numpy as np
 import math
-from numpy.fft import ifft, fft
+from numpy.fft import ifft, fft, fftn, ifftn
 import matplotlib.pyplot as plt
 from scipy.integrate import ode
+from itertools import zip_longest, repeat
 
 
 def pseudospectral_factor(interval, grid_points, power):
@@ -26,6 +27,17 @@ def pseudospectral_factor(interval, grid_points, power):
     return x, kxx
 
 
+def pseudospectral_factor_multi(intervals, grid_points_list, power):
+    # get grid points and the pseudospectral coefficients for the given derivative in spatial domain
+    # in each dimension
+    x, kxx = [], []
+    for interval, grid_points in zip_longest(intervals, grid_points_list, fillvalue=grid_points_list[-1]):
+        curr_x, curr_k = pseudospectral_factor(interval, grid_points, power)
+        x.append(curr_x)
+        kxx.append(curr_k)
+    return x, np.meshgrid(*x, sparse=True), np.meshgrid(*kxx, sparse=True)
+
+
 # returns (x,y) with x being the 1D grid points and y the pseudospectral derivative at
 # these grid points: y = (d/dx)^power of u. Requires u to be periodical in the interval
 def spectral_derivative(interval, grid_points, u, power):
@@ -34,62 +46,128 @@ def spectral_derivative(interval, grid_points, u, power):
 
 
 # u_t(t,x) = alpha * u_xx(t,x), u(t0,x)=u0(x), alpha > 0
-def heat_solution(interval, grid_points, t0, u0, alpha, t_end, dt):
+def heat_solution(intervals, grid_points_list, t0, u0, alpha, wanted_times):
     assert alpha > 0.
-    assert dt > 0.
-    assert t_end >= t0
-    # get grid points and the pseudospectral coefficients for the second order derivative in spatial domain
-    x, kxx = pseudospectral_factor(interval, grid_points, 2)
+
+    # plural 's' means a list, so list of x coordinates, list of meshgrid coordinates, list of fourier coefficients
+    xs, xxs, ks = pseudospectral_factor_multi(intervals, grid_points_list, 2)
 
     # variables ending in underscore note that the values are considered to be in fourier space
+    y0 = u0(xxs)
+    y0_ = fftn(y0)  # starting condition in fourier space and evaluated at grid
 
-    y0 = u0(x)
-    y0_ = fft(y0)  # starting condition in fourier space and evaluated at grid
-
-    times, solutions = [t0], [y0]
-    for t in np.arange(t0 + dt, t_end + dt, dt):
+    times = list(filter(lambda time_check: time_check >= t0, wanted_times))
+    solutions = []
+    for t in times:
         # for all j solve d/dt u_hat(j; t) = -j*j*u_hat(j; t) and starting condition u_hat(j;0)=y0_(j)
         # here we are in the position to know the exact solution!
-        u_hat_ = y0_ * np.exp(alpha * kxx * t)  # solution at time t with starting value y0_, all in fourier space
 
-        y = ifft(u_hat_).real
-        times.append(t)
+        # solution at time t with starting value y0_, all in fourier space
+        u_hat_ = y0_ * np.exp(alpha * sum(k for k in ks) * t)
+
+        y = ifftn(u_hat_).real
         solutions.append(y)
-    return x, times, solutions
+    return xs, times, solutions
 
 
-def wave_solution(interval, grid_points, g, dt, t0, y0, t_end):
-    def f(t, y):
-        return y
+# u_tt(t,x) = alpha * u_xx(t,x), u(t0,x)=u0(x), u_t(t0,x)=u0_t(x), alpha > 0
+def wave_solution(intervals, grid_points_list, t0, u0, u0t, alpha, wanted_times):
+    assert alpha > 0.
 
-    r = ode(f).set_integrator('zvode', method='adams')  # 'zvode' for complex valued problems, 'adams' for non-stiff
-    r.set_initial_value(y0, t0)
+    # plural 's' means a list, so list of x coordinates, list of meshgrid coordinates, list of fourier coefficients
+    xs, xxs, ks = pseudospectral_factor_multi(intervals, grid_points_list, 2)
 
-    while r.successful() and r.t < t_end:
-        r.integrate(r.t + dt)
-        print("{} {}".format(r.t, r.y))
+    # variables ending in underscore note that the values are considered to be in fourier space
+    y0 = u0(xxs)
+    y0_ = fftn(y0)  # starting condition in fourier space and evaluated at grid
+    y0t_ = fftn(u0t(xxs))
 
+    times = list(filter(lambda time_check: time_check >= t0, wanted_times))
+    solutions = []
+    for t in times:
+        # for all j solve (d/dt)^2 u_hat(j; t) = -j*j*u_hat(j; t) and starting conditions u_hat(j;0)=y0_(j),
+        # (d/dt)u_hat(j;0)=y0t_(j)
+        # here we are in the position to know the exact solution for this linear ordinary differential equation!
+
+        # solution at time t with starting value y0_ and y0t_, all in fourier space
+        sum_ks = sum(k for k in ks)
+        c1_ = y0_
+        # will produce a warning currently because of dividing by zero at some points, c2_ should be zero there
+        c2_ = np.nan_to_num(y0t_ / (-1j * sum_ks))  # for the very few points where k is zero set c2_ to zero
+
+        u_hat_ = c1_ * np.cosh(-1j * sum_ks * t) + c2_ * np.sinh(-1j * sum_ks * t)
+
+        y = ifftn(u_hat_)
+        solutions.append(y)
+    return xs, times, solutions
 
 if __name__ == "__main__":
     test_derivative = False
-    test_heat = True
+    test_heat = False
+    test_wave = True
+
+    if test_wave:
+        grid_n = 128  # power of 2 for best performance of fft
+        alpha = 1  # > 0
+        dimension = 1  # plotting only supported for one or two dimensional, higher dimension will require lower grid_n
+        domain = list(repeat([-math.pi, math.pi], dimension))  # intervals with periodic boundary conditions, so a torus
+        wanted_times = [0, 0.5, 1, 2, 3, 4]  # times to evaluate solution for and plot it
+
+        def start_position(xs):
+            return np.sin(sum(x for x in xs))
+
+        def start_velocity(xs):
+            return np.cos(sum(x for x in xs))
+            # return sum(np.zeros(shape=x.shape) for x in xs)
+
+        def reference(xs, t):
+            # for start_velocity zero and start_position sinus, this is the d'Alembert reference solution
+            return np.sin(sum(x for x in xs) - t) / 2 + np.sin(sum(x for x in xs) + t) / 2
+
+        x_result, t_result, y_result = wave_solution(domain, [grid_n],
+                                                     0, start_position, start_velocity, alpha, wanted_times)
+        if len(x_result) == 1:  # 1D plots
+            # all times in one figure
+
+            # plt.plot(*x_result, reference(x_result, t_result[1]),
+            #         label="Reference solution at time=" + str(t_result[1]))
+            for time, sol in zip(t_result, y_result):
+                plt.plot(*x_result, sol.real, label="Solution at time=" + str(time))
+            plt.legend()
+            plt.title("Wave equation solution by pseudospectral spatial method and exact time solution\nwith N="
+                      + str(grid_n) + " gridpoints")
+            plt.show()
 
     if test_heat:
-        dt = 0.1
-        n = 512
-        alpha = 0.01
+        grid_n = 128  # power of 2 for best performance of fft
+        alpha = 0.01  # > 0
+        dimension = 2  # plotting only supported for one or two dimensional, higher dimension will require lower grid_n
+        domain = list(repeat([-math.pi, math.pi], dimension))  # intervals with periodic boundary conditions, so a torus
+        wanted_times = [0, 5, 10, 30, 50, 100, 500]  # times to evaluate solution for and plot it
 
-        def start_condition(x):
-            # return np.sin(x)
-            return 1 / np.cosh(10 * x / math.pi) ** 2
-            # return np.where(-1 < x, np.where(x > 1, np.ones(shape=x.shape), 0), 1)  # discontinuous block
-        x, t, y = heat_solution([-math.pi, math.pi], n, 0, start_condition, alpha, 5., dt)
-        for time, sol in zip(t, y):
-            if time in [0, 1, 2, 3, 4, 5]:
-                plt.plot(x, sol, label="Solution at time=" + str(time))
-        plt.legend()
-        plt.title("Heat equation solution by pseudospectral spatial method and exact time solution\nwith N=" + str(n) + " gridpoints and starting value 1/cosh(10x/pi)^2")
-        plt.show()
+        # starting condition for homogeneous heat equation with periodic boundary equation in given domain
+        def start_condition(xs):
+            # return -np.sin(sum(x for x in xs))
+            return 1 / np.cosh(10 * sum(x for x in xs) / math.pi) ** 2
+            # return np.where(-1 < x, np.where(x > 1, np.ones(shape=x.shape), 0), 1)  # discontinuous block in 1D
+        x_result, t_result, y_result = heat_solution(domain, [grid_n],
+                                                     0, start_condition, alpha, wanted_times)
+
+        if len(x_result) == 1:  # 1D plots
+            # all times in one figure
+            for time, sol in zip(t_result, y_result):
+                plt.plot(*x_result, sol, label="Solution at time=" + str(time))
+            plt.legend()
+            plt.title("Heat equation solution by pseudospectral spatial method and exact time solution\nwith N="
+                      + str(grid_n) + " gridpoints")
+            plt.show()
+        elif len(x_result) == 2:  # 2D plots
+            # one figure per time
+            for time, sol in zip(t_result, y_result):
+                plt.figure()
+                plt.pcolormesh(*x_result, sol, vmin=-1, vmax=1)  # fix color bar range over different figures
+                plt.colorbar()
+                plt.show()
 
     if test_derivative:
         bound = [-1, 1]
